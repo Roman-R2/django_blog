94c7f8193cdd7bb53f87f53f5ffda710c7ea4f40
Roman <slyusarrv@yandex.ru>
2021-10-28 16:44:31 +0500

    some edits

--- siteblog/urls.py
+++ siteblog/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
 ]
+
+# Если мы в режиме дебага, то скажем django где брать загруженные файлы картинок и пр.
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
